f753e66d216ac86cbc156f29e19985316358fe7c
SukruYuksel <yksl3743dc@gmail.com>
2024-01-03 20:43:21 +0100

    Local1 ve Local2 eklendi

---
+++ src/Local1.java
@@ -0,0 +1,10 @@
+public class Local1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Git Test");
+        System.out.println("Yeni Satir eklendi");
+        System.out.println("Yeni Test");
+
+    }
+}
---
+++ src/Local2.java
@@ -0,0 +1,9 @@
+public class Local2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Test1");
+        System.out.println("Test2");
+
+    }
+}
