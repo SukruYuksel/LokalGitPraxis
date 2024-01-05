67eec8b3d5cd38cdd642fc8f6cbd8c9598aea4c9
Sükrü Yüksel <65497504+SukruYuksel@users.noreply.github.com>
2024-01-05 19:45:24 +0100

    Remoe da Sükrü dosyasi güncellendi

--- src/Sukru.java
+++ src/Sukru.java
@@ -1,5 +1,8 @@
 public class Sukru {
     public static void main(String[] args) {
         System.out.println("Sükrü branch eklenen code");
+//Yeni dosyalar eklendi
+        System.out.println("Sükrü Remotuna dosya eklendi");
+    
     }
 }
